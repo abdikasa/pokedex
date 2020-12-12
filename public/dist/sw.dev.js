6121b7963cf196a15c24a4c73d2a730b1d800994
abdikasa <abdikasa@sheridancollege.ca>
2020-12-12 18:28:23 -0500

    Testing out service worker for online and offline

---
+++ public/dist/sw.dev.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var CACHE_NAME = "my_cache";
+self.addEventListener("install", function (e) {
+  console.log("About to install the service worker babyy");
+  e.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+    return cache.addAll(["/", "/index.html", "static/js/bundle.js"]).then(function () {
+      return self.skipWaiting();
+    });
+  }));
+});
+self.addEventListener("activate", function (event) {
+  console.log("about to activate my service worker");
+  event.waitUntil(self.clients.claim);
+});
+self.addEventListener("fetch", function (event) {
+  console.log("about to fetch ".concat(event.request.url));
+
+  if (navigator.onLine) {
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest).then(function (response) {
+      if (!response || response.status !== 200 || response.type !== "basic") {
+        return response;
+      }
+
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function (cache) {
+        cache.put(event.request, responseToCache);
+      });
+      return response;
+    });
+  } else {
+    event.respondWith(caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+    }));
+  }
+});
---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "my_cache";
+
+self.addEventListener("install", (e) => {
+  console.log("About to install the service worker babyy");
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache
+        .addAll(["/", "/index.html", "static/js/bundle.js"])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("about to activate my service worker");
+  event.waitUntil(self.clients.claim);
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log(`about to fetch ${event.request.url}`);
+  if (navigator.onLine) {
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest).then(function (response) {
+      if (!response || response.status !== 200 || response.type !== "basic") {
+        return response;
+      }
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function (cache) {
+        cache.put(event.request, responseToCache);
+      });
+      return response;
+    });
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        }
+      })
+    );
+  }
+});
