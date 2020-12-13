5eedb74fed3d2b391140ce14e74ce03c2897d872
abdikasa <abdikasa@sheridancollege.ca>
2020-12-12 20:17:07 -0500

    async/await service worker

--- public/sw.js
+++ public/sw.js
@@ -16,7 +16,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim);
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", async function (event) {
   console.log(`about to fetch ${event.request.url}`);
 
   if (
@@ -26,17 +26,13 @@ self.addEventListener("fetch", function (event) {
     return;
 
   if (navigator.onLine) {
-    var fetchRequest = event.request.clone();
-    return fetch(fetchRequest).then(function (response) {
-      if (!response || response.status !== 200 || response.type !== "basic") {
-        return response;
-      }
-      var responseToCache = response.clone();
-      caches.open(CACHE_NAME).then(function (cache) {
-        cache.put(event.request, responseToCache);
-      });
+    const response = await fetch(event.request);
+    if (!response || response.status !== 200 || response.type !== "basic") {
       return response;
-    });
+    }
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(event.request, response.clone());
+    return response;
   } else {
     event.respondWith(
       caches.match(event.request).then(function (response) {
@@ -47,3 +43,53 @@ self.addEventListener("fetch", function (event) {
     );
   }
 });
+
+// const CACHE_NAME = "my_cache";
+
+// self.addEventListener("install", (e) => {
+//   console.log("About to install the service worker babyy");
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache
+//         .addAll(["/", "/index.html", "static/js/bundle.js"])
+//         .then(() => self.skipWaiting());
+//     })
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   console.log("about to activate my service worker");
+//   event.waitUntil(self.clients.claim);
+// });
+
+// self.addEventListener("fetch", function (event) {
+//   console.log(`about to fetch ${event.request.url}`);
+
+//   if (
+//     event.request.cache === "only-if-cached" &&
+//     event.request.mode !== "same-origin"
+//   )
+//     return;
+
+//   if (navigator.onLine) {
+//     var fetchRequest = event.request.clone();
+//     return fetch(fetchRequest).then(function (response) {
+//       if (!response || response.status !== 200 || response.type !== "basic") {
+//         return response;
+//       }
+//       var responseToCache = response.clone();
+//       caches.open(CACHE_NAME).then(function (cache) {
+//         cache.put(event.request, responseToCache);
+//       });
+//       return response;
+//     });
+//   } else {
+//     event.respondWith(
+//       caches.match(event.request).then(function (response) {
+//         if (response) {
+//           return response;
+//         }
+//       })
+//     );
+//   }
+// });
