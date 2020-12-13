1ccfb8bd3efc0e48fec49dd2796667a11cb5bf74
abdikasa <abdikasa@sheridancollege.ca>
2020-12-13 14:27:14 -0500

    Switched caches.addAll to add

--- public/dist/sw.dev.js
+++ public/dist/sw.dev.js
@@ -1,11 +1,14 @@
 "use strict";
 
 var CACHE_NAME = "my_cache";
+var urlsToCache = ["/", "./index.html", "static/js/bundle.js"];
 self.addEventListener("install", function (e) {
   console.log("About to install the service worker babyy");
   e.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
-    return cache.addAll(["/", "./index.html", "static/js/bundle.js"]).then(function () {
-      return self.skipWaiting();
+    urlsToCache.forEach(function (url) {
+      return cache.add(url).then(function () {
+        return self.skipWaiting();
+      });
     });
   }));
 });
@@ -13,66 +16,56 @@ self.addEventListener("activate", function (event) {
   console.log("about to activate my service worker");
   event.waitUntil(self.clients.claim);
 });
-self.addEventListener("fetch", function _callee(event) {
-  var response, cache;
-  return regeneratorRuntime.async(function _callee$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          if (!(event.request.cache === "only-if-cached" && event.request.mode !== "same-origin")) {
-            _context.next = 2;
-            break;
-          }
+self.addEventListener("fetch", function (event) {
+  console.log("about to fetch ".concat(event.request.url));
+  if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
 
-          return _context.abrupt("return");
-
-        case 2:
-          response = null;
-
-          if (!navigator.onLine) {
-            _context.next = 17;
-            break;
-          }
-
-          _context.next = 6;
-          return regeneratorRuntime.awrap(fetch(event.request));
-
-        case 6:
-          response = _context.sent;
-
-          if (!(!response || response.status !== 200 || response.type !== "basic")) {
-            _context.next = 9;
-            break;
-          }
-
-          return _context.abrupt("return", response);
-
-        case 9:
-          _context.next = 11;
-          return regeneratorRuntime.awrap(caches.open(CACHE_NAME));
-
-        case 11:
-          cache = _context.sent;
-          _context.next = 14;
-          return regeneratorRuntime.awrap(cache.put(event.request, response.clone()));
-
-        case 14:
-          return _context.abrupt("return", response);
-
-        case 17:
-          event.respondWith(caches.match(event.request).then(function (response) {
-            if (response) {
-              return response;
-            }
-          }));
-
-        case 18:
-        case "end":
-          return _context.stop();
+  if (navigator.onLine) {
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest).then(function (response) {
+      if (!response || response.status !== 200 || response.type !== "basic") {
+        return response;
       }
-    }
-  });
-}); // const CACHE_NAME = "my_cache";
+
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function (cache) {
+        cache.put(event.request, responseToCache);
+      });
+      return response;
+    });
+  } else {
+    event.respondWith(caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+    }));
+  }
+}); // self.addEventListener("fetch", async function (event) {
+//   if (
+//     event.request.cache === "only-if-cached" &&
+//     event.request.mode !== "same-origin"
+//   )
+//     return;
+//   let response = null;
+//   if (navigator.onLine) {
+//     response = await fetch(event.request);
+//     if (!response || response.status !== 200 || response.type !== "basic") {
+//       return response;
+//     }
+//     const cache = await caches.open(CACHE_NAME);
+//     await cache.put(event.request, response.clone());
+//     return response;
+//   } else {
+//     event.respondWith(
+//       caches.match(event.request).then(function (response) {
+//         if (response) {
+//           return response;
+//         }
+//       })
+//     );
+//   }
+// });
+// const CACHE_NAME = "my_cache";
 // self.addEventListener("install", (e) => {
 //   console.log("About to install the service worker babyy");
 //   e.waitUntil(
--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,14 @@
 const CACHE_NAME = "my_cache";
 
+const urlsToCache = ["/", "./index.html", "static/js/bundle.js"];
+
 self.addEventListener("install", (e) => {
   console.log("About to install the service worker babyy");
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache
-        .addAll(["/", "./index.html", "static/js/bundle.js"])
-        .then(() => self.skipWaiting());
+      urlsToCache.forEach((url) => {
+        return cache.add(url).then(() => self.skipWaiting());
+      });
     })
   );
 });
@@ -16,22 +18,27 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim);
 });
 
-self.addEventListener("fetch", async function (event) {
+self.addEventListener("fetch", function (event) {
+  console.log(`about to fetch ${event.request.url}`);
+
   if (
     event.request.cache === "only-if-cached" &&
     event.request.mode !== "same-origin"
   )
     return;
 
-  let response = null;
   if (navigator.onLine) {
-    response = await fetch(event.request);
-    if (!response || response.status !== 200 || response.type !== "basic") {
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest).then(function (response) {
+      if (!response || response.status !== 200 || response.type !== "basic") {
+        return response;
+      }
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function (cache) {
+        cache.put(event.request, responseToCache);
+      });
       return response;
-    }
-    const cache = await caches.open(CACHE_NAME);
-    await cache.put(event.request, response.clone());
-    return response;
+    });
   } else {
     event.respondWith(
       caches.match(event.request).then(function (response) {
@@ -42,6 +49,33 @@ self.addEventListener("fetch", async function (event) {
     );
   }
 });
+
+// self.addEventListener("fetch", async function (event) {
+//   if (
+//     event.request.cache === "only-if-cached" &&
+//     event.request.mode !== "same-origin"
+//   )
+//     return;
+
+//   let response = null;
+//   if (navigator.onLine) {
+//     response = await fetch(event.request);
+//     if (!response || response.status !== 200 || response.type !== "basic") {
+//       return response;
+//     }
+//     const cache = await caches.open(CACHE_NAME);
+//     await cache.put(event.request, response.clone());
+//     return response;
+//   } else {
+//     event.respondWith(
+//       caches.match(event.request).then(function (response) {
+//         if (response) {
+//           return response;
+//         }
+//       })
+//     );
+//   }
+// });
 
 // const CACHE_NAME = "my_cache";
 
